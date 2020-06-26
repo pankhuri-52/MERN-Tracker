e1866758358247c131110a83f954e637e7b0ac5e
Pankhuri <pankhuritrikha@gmail.com>
2020-06-26 11:07:12 +0530

    All components are ready with their basic structure

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
        <br/>
-        <Route path="/" component={ExercisesList} />
+        <Route path="/" exact component={ExercisesList} />
         <Route path="/edit/:id" component={EditExercise} />
         <Route path="/create" component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
